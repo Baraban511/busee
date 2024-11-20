42cd4a99d8fbcda36b4391d1722f65edc8762682
barab <94920370+Baraban511@users.noreply.github.com>
2024-11-20 14:48:27 +0100

    Add countdown

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,6 @@ import { Button, Text, View, useWindowDimensions } from "react-native";
 import React, { useEffect, useState } from 'react';
 import RenderHtml from '@builder.io/react-native-render-html';
 import { DateTime } from "luxon";
-import { parse } from "@babel/core";
 
 export default function App() {
   return (
@@ -23,8 +22,8 @@ function Disruptions() {
   const { width } = useWindowDimensions();
   const [disruptionsMessage, setDisruptionsMessage] = useState("loading...");
   const getDisruptions = async () => {
-    var disruptions = await fetch("https://sytral.api.instant-system.com/InstantCore/v4/networks/57/disruptions?channelType=MOBILE&entityIds=line:tcl:C11");
-    disruptions = await disruptions.json();
+    let response = await fetch("https://sytral.api.instant-system.com/InstantCore/v4/networks/57/disruptions?channelType=MOBILE&entityIds=line:tcl:C11");
+    var disruptions = await response.json();
     if (disruptions.disruptions[0]) {
       setDisruptionsMessage(disruptions.disruptions[0].messages[0].translations[0].content);
     }
@@ -42,58 +41,70 @@ function Disruptions() {
 }
 
 function NextBus() {
+  const [busCountdown, setBusCountdown] = useState(0);
+  const [dataFetchInterval, setDataFetchInterval] = useState(60000);
 
-  const [nextBusInterval, setNextBusInterval] = useState("loading...");
-  const [fetchInterval, setFetchInterval] = useState(60000);
 
-  function parseTime(time: DateTime) {
-    var text = "";
-    if (time.minute > 0) {
-      text += time.minute + "min ";
-    }
-    if (time.second > 0) {
-      text += time.second + "s";
-    }
-    return text;
-  }
-
-  const updateBusTime = async () => {
+  const updateBusInterval = async () => {
+    console.log("updateBusInterval");
     var busTime = await getNextBusArrival();
     if (busTime) {
-      var busInterval = DateTime.fromMillis(busTime.diffNow("milliseconds").milliseconds);
-      setNextBusInterval(parseTime(busInterval));
-      if (busInterval.minute < 2) {
-        setFetchInterval(1000); //Une seconde
+      let busInterval = DateTime.fromMillis(busTime.diffNow("milliseconds").milliseconds);
+      setBusCountdown(busInterval.toMillis());
+      if (busInterval.minute < 0) {
+        setDataFetchInterval(5000); //5 secondes
       }
       else {
-        setFetchInterval(60000); //Une minute
+        setDataFetchInterval(30000); //30 secondes
       }
       return;
     }
-    setNextBusInterval("pas de bus");
   };
 
   useEffect(() => {
-    updateBusTime();
     const interval = setInterval(() => {
-      updateBusTime();
-    }, fetchInterval);
+      setBusCountdown((prevCountdown) => prevCountdown - 1000);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  useEffect(() => {
+    updateBusInterval();
+    const interval = setInterval(() => {
+      updateBusInterval();
+    }, dataFetchInterval);
     return () => clearInterval(interval);
   }, []);
 
   return (<>
-    <Text>Prochain bus dans {nextBusInterval}</Text>
-    <Button title="Rafraichir" onPress={() => updateBusTime()} /></>
+    <Text>Prochain bus dans {parseBusCountdown(busCountdown)}</Text>
+    <Button title="Rafraichir" onPress={() => updateBusInterval()} /></>
   )
 }
 
 async function getNextBusArrival() {
-  var nextBus = await fetch("https://sytral.api.instant-system.com/InstantCore/v4/networks/57/lines/line:tcl:C11/stopPoints/stop_point:tcl:SP:48329/schedules?duration=3600&dataFreshness=realtime");
-  nextBus = await nextBus.json();
+  let response = await fetch("https://sytral.api.instant-system.com/InstantCore/v4/networks/57/lines/line:tcl:C11/stopPoints/stop_point:tcl:SP:48329/schedules?duration=3600&dataFreshness=realtime");
+  var nextBus = await response.json();
   if (nextBus.stopSchedules[0].dateTimes[0]) {
     return DateTime.fromISO(nextBus.stopSchedules[0].dateTimes[0].dateTime);
   }
   else {
     return undefined;
   }
+}
+
+function parseBusCountdown(millisecond: number) {
+  var text = "";
+  var duration = DateTime.fromMillis(millisecond);
+  if (duration.minute < 0) {
+    return "maintenant";
+  }
+  if (duration.minute > 0) {
+    text += duration.minute + " minute" + (duration.minute > 1 ? "s " : " ");
+  }
+  if (duration.second > 0) {
+    text += duration.second + " seconde" + (duration.second > 1 ? "s" : "");
+  }
+  return text;
 }
